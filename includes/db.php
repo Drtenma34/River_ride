065bc49a5b75e7eb4b777a2248aa615abe035d1f
Redak92 <bastienguibert98@gmail.com>
2023-08-24 19:04:04 +0200

    db recorrigée + stages

--- includes/db.php
+++ includes/db.php
@@ -1,13 +1,9 @@
 <?php
-
-try {
-    $bdd = new PDO(
-        'mysql:host=5.196.27.192; 
-        dbname=Riverride',
-        'root',
-        'MysqlRootRiverSide',
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-    );
-} catch (Exception $e) {
-    die($e->getMessage());
+// Connexion à la base de données
+try{
+	$bdd = new PDO('mysql:host=localhost;dbname=Riverride', 'root', 'root', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 }
+catch(Exception $e){
+	die('Erreur : ' . $e->getMessage());
+}
+?>
