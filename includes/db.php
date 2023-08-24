c422d96469dad3a0c8be5aaa6136f389f780e86c
DrTenma <113189733+Drtenma34@users.noreply.github.com>
2023-08-24 20:26:54 +0200

    DB MARCHE !!!

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,10 @@
 <?php
 // Connexion à la base de données
 try{
-	$bdd = new PDO('mysql:host=localhost;dbname=Riverride', 'root', 'root', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+	$bdd = new PDO('mysql:host=5.196.27.192;dbname=riverride', 'root', 'todomundo34river', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 }
 catch(Exception $e){
 	die('Erreur : ' . $e->getMessage());
 }
 ?>
+
