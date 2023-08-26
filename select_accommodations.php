020a49ee76b868034001f3c80cbb84481ebc3067
Redak92 <bastienguibert98@gmail.com>
2023-08-26 16:53:33 +0200

    pousse

--- select_accommodations.php
+++ select_accommodations.php
@@ -26,28 +26,10 @@ if (!empty($selected_stages)) {
     <title>Hébergements disponibles</title>
     <?php include("includes/head.php"); ?>
     <style>
-        /* Existing CSS styles for the accommodations display */
-        .card-img-top {
-            width: 200px;
-            height: 200px;
-            object-fit: cover;
-        }
+        /* ... [Existing styles] ... */
 
-        /* ... [you might want to add other styles from the previous code or customize as needed] ... */
-
-        .card {
-            box-shadow: 0 4px 8px rgba(0,0,0,0.1);
-            transition: transform .2s;
-            padding: 20px;
-            border-radius: 5px;
-            background-color: #fff;
-            width: 220px;
-            text-align: center;
-            margin-bottom: 20px;
-        }
-
-        .card:hover {
-            transform: scale(1.05);
+        .date-input {
+            margin-top: 10px;
         }
     </style>
 </head>
@@ -55,40 +37,48 @@ if (!empty($selected_stages)) {
     <?php include("includes/header_menu.php"); ?>
     <main>
         <h2>Hébergements disponibles</h2>
-        <?php
-        foreach ($stages as $stage) {
-            echo '<h3>Étape: ' . $stage['nom'] . '</h3>';
-            echo '<div class="card-container">';
 
-            // Fetching accommodations for this specific stage
-            $accommodations = [];
-            try {
-                $stmt = $bdd->prepare("SELECT * FROM accommodations WHERE travel_stage_id = ?");
-                $stmt->execute([$stage['id']]);
-                $accommodations = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            } catch (PDOException $e) {
-                die("Erreur: " . $e->getMessage());
+        <form action="select_services.php" method="post">
+            <?php
+            foreach ($stages as $stage) {
+                echo '<h3>Étape: ' . $stage['nom'] . '</h3>';
+                echo '<div class="card-container">';
+
+                // Fetching accommodations for this specific stage
+                $accommodations = [];
+                try {
+                    $stmt = $bdd->prepare("SELECT * FROM accommodations WHERE travel_stage_id = ?");
+                    $stmt->execute([$stage['id']]);
+                    $accommodations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                } catch (PDOException $e) {
+                    die("Erreur: " . $e->getMessage());
+                }
+
+                foreach ($accommodations as $accommodation) {
+                    echo '<div class="card">';
+                    echo '<img class="card-img-top" src="' . $accommodation['photo'] . '" alt="' . $accommodation['nom'] . '">';
+                    echo '<h3 class="card-title">' . $accommodation['nom'] . '</h3>';
+                    echo '<p>Adresse: ' . $accommodation['adresse'] . '</p>';
+                    echo '<p>Prix par nuit: ' . $accommodation['price_per_night'] . '€</p>';
+
+                    echo '<div class="date-input">';
+                    echo '<label>De: <input type="date" name="dates[' . $accommodation['id'] . '][from]"></label>';
+                    echo '<label>À: <input type="date" name="dates[' . $accommodation['id'] . '][to]"></label>';
+                    echo '<label><input type="checkbox" name="selected_hotels[]" value="' . $accommodation['id'] . '"> Sélectionner</label>';
+                    echo '</div>';
+
+                    echo '</div>';
+                }
+
+                echo '</div>';  // End of card-container
             }
 
-            foreach ($accommodations as $accommodation) {
-                echo '<div class="card">';
-                echo '<img class="card-img-top" src="' . $accommodation['photo'] . '" alt="' . $accommodation['nom'] . '">';
-                echo '<h3 class="card-title">' . $accommodation['nom'] . '</h3>';
-                echo '<p>Adresse: ' . $accommodation['adresse'] . '</p>';
-                echo '<p>Prix par nuit: ' . $accommodation['price_per_night'] . '€</p>';
+            echo '<div style="text-align: center; margin-top: 20px;">';
+            echo '<input type="submit" value="Continuer avec les dates sélectionnées">';
+            echo '</div>';
+            ?>
+        </form>
 
-                // Form for hotel selection
-                echo '<form action="select_services.php" method="post">';
-                echo '<input type="hidden" name="selected_hotel" value="' . $accommodation['id'] . '">';
-                echo '<input type="submit" value="Sélectionner">';
-                echo '</form>';
-
-                echo '</div>';
-            }
-
-            echo '</div>';  // End of card-container
-        }
-        ?>
     </main>
     <footer>
         <p>&copy; 2023 Tous droits réservés.</p>
---
+++ select_services.php
@@ -0,0 +1,71 @@
+<?php
+include("includes/db.php");  // Assuming that the file `db.php` initializes a PDO connection
+
+// Get selected hotels and dates from the POST data
+$selected_hotels = isset($_POST['selected_hotels']) ? $_POST['selected_hotels'] : [];
+$dates = isset($_POST['dates']) ? $_POST['dates'] : [];
+
+// Fetch the details of selected hotels
+$hotel_details = [];
+if (!empty($selected_hotels)) {
+    try {
+        $placeholders = str_repeat('?,', count($selected_hotels) - 1) . '?';
+        $stmt = $bdd->prepare("SELECT * FROM accommodations WHERE id IN ($placeholders)");
+        $stmt->execute($selected_hotels);
+        $hotel_details = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        die("Erreur: " . $e->getMessage());
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Services supplémentaires</title>
+    <?php include("includes/head.php"); ?>
+    <style>
+        /* You can add additional styles here */
+    </style>
+</head>
+<body>
+    <?php include("includes/header_menu.php"); ?>
+    <main>
+        <h2>Services supplémentaires</h2>
+        <p>Choisissez des suppléments de bagage pour vos réservations d'hôtel.</p>
+        
+        <form action="finalize_booking.php" method="post"> <!-- Assuming there's a page named finalize_booking.php where the booking will be finalized -->
+            <?php
+            foreach ($hotel_details as $hotel) {
+                echo '<div class="hotel-section">';
+                echo '<h3>' . $hotel['nom'] . '</h3>';
+                echo '<p>Adresse: ' . $hotel['adresse'] . '</p>';
+                echo '<p>Réservé du ' . $dates[$hotel['id']]['from'] . ' au ' . $dates[$hotel['id']]['to'] . '</p>';
+                
+                echo '<div class="baggage-option">';
+                echo '<label>Supplément bagage: ';
+                echo '<select name="baggage_supplement[' . $hotel['id'] . ']">';
+                echo '<option value="0">Aucun</option>';
+                echo '<option value="10">10€ - 10kg supplémentaires</option>';
+                echo '<option value="20">20€ - 20kg supplémentaires</option>';
+                echo '</select>';
+                echo '</label>';
+                echo '</div>';
+
+                echo '</div>';
+            }
+            ?>
+
+            <div style="text-align: center; margin-top: 20px;">
+                <input type="submit" value="Finaliser la réservation">
+            </div>
+        </form>
+
+    </main>
+    <footer>
+        <p>&copy; 2023 Tous droits réservés.</p>
+    </footer>
+</body>
+</html>
